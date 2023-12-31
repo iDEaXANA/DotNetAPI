6ec0e467ff1a58937d3d7892a868adf09a71f682
Bilal MB <bmbenkada@gmail.com>
2023-12-31 21:37:24 +0000

    Hi, I'm Day 4

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,6 +25,7 @@ public class WeatherForecastController : ControllerBase
 
 }
 
+
 public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
