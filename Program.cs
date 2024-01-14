5a1ff2681732662a2ab9e3f81f57a9417bca577f
Bilal MB <bmbenkada@gmail.com>
2024-01-14 13:39:56 +0000

    Added CORS Policy. Will be developing API, creating user models and linking them to controllers.

--- Program.cs
+++ Program.cs
@@ -7,16 +7,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors((options) =>
+    {
+        // Development
+        options.AddPolicy("DevCors", (corsBuilder) =>
+        {
+            corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000") //Angular, React, Vue 
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+        });
+        // Deployment
+        options.AddPolicy("ProdCors", (corsBuilder) =>
+        {
+            corsBuilder.WithOrigins("https://myProductionSite.com") //Front-End.
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+        });
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 else
 {
+    app.UseCors("ProdCors");
     app.UseHttpsRedirection(); // More secure method but no need whilst dev
 }
 app.MapControllers();
