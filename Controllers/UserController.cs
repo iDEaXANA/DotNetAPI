a735d5ba3d5ff757036efeea591ed217ed63128a
Bilal MB <bmbenkada@gmail.com>
2024-01-15 20:03:56 +0000

    Created a POST and PUT route. Tested it with Swagger UI and used ADS to debug SQL errors

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -17,16 +17,92 @@ public class UserController : ControllerBase
         return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
     }
 
-    [HttpGet("GetUsers/{testValue}")]
-    public IActionResult GetUsers(string testValue) // string[] can be replaced with IActionResult
+    [HttpGet("GetUsers")]
+    public IEnumerable<User> GetUsers() // string[] can be replaced with IActionResult
     {
-        string[] responseArray = new string[] {
-            "test1",
-            "test2",
-            testValue
-        };
-        return new OkObjectResult(responseArray); //Use ok()
+        string sql = @"
+            SELECT [UserId],
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active] 
+            FROM TutorialAppSchema.Users";
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        return users;
+        // string[] responseArray = new string[] {
+        //     "test1",
+        //     "test2",
+        //     testValue
+        // };
+        // return new OkObjectResult(responseArray); //Use ok()
     }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    public User GetSingleUser(int userId)
+    {
+        string sql = @"
+            SELECT [UserId],
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active] 
+            FROM TutorialAppSchema.Users
+                WHERE UserId = " + userId.ToString();
+        User users = _dapper.LoadDataSingle<User>(sql);
+        return users;
+    }
+
+    [HttpPut("EditUser")]
+    public IActionResult EditUser(User user)
+    {
+        string sql = @"
+        UPDATE TutorialAppSchema.Users
+            SET [FirstName] = '" + user.FirstName +
+                "', [LastName] = '" + user.LastName +
+                "', [Email] = '" + user.Email +
+                "', [Gender] = '" + user.Gender +
+                "', [Active] = '" + user.Active +
+            "' WHERE UserId = " + user.UserId;
+
+        Console.WriteLine(sql);
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to Update User");
+
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(User user)
+    {
+        string sql = @"INSERT INTO TutorialAppSchema.Users(
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active]
+            ) VALUES (" +
+                "'" + user.FirstName +
+                "', '" + user.LastName +
+                "', '" + user.Email +
+                "', '" + user.Gender +
+                "', '" + user.Active +
+            "')";
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to Add User");
+    }
+
+
 
 }
 
