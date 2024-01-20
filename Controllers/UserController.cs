56a84ef0c03302f7c3c98a2813153044eaa18cb1
Bilal MB <bmbenkada@gmail.com>
2024-01-20 16:05:49 +0000

    Achievement Unlocked: Completed CRUD. Next: EF, Repo Pattern

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -105,7 +105,22 @@ public class UserController : ControllerBase
         throw new Exception("Failed to Add User");
     }
 
+    [HttpDelete("DeleteUser/{userId}")]
+    public IActionResult DeleteUser(int userId)
+    {
+        string sql = @"
+        DELETE FROM TutorialAppSchema.Users 
+            WHERE UserId = " + userId.ToString();
 
+        Console.WriteLine(sql);
 
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to Delete User");
+
+    }
 }
 
