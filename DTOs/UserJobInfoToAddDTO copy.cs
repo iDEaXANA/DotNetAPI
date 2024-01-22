7d3e40f03887101a684581c166be79fcd8a5059f
Bilal MB <bmbenkada@gmail.com>
2024-01-22 13:33:52 +0000

    Added HTTP requests and DTO of UserJobInfo and tested all endpoints. AddUserJobInfo doesn't assign a UserId and DeleteUserJobInfo logs a deletion after a second attempt but still shows the entry on Azure

---
+++ DTOs/UserJobInfoToAddDTO copy.cs
@@ -0,0 +1,8 @@
+namespace DotnetAPI.DTOs
+{
+    public partial class UserJobInfoToAddDTO
+    {
+        public string JobTitle { get; set; } = "";
+        public string Department { get; set; } = "";
+    }
+}
