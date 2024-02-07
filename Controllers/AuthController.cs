232bf155a1e279e20002586b1ebaad53b1ba296e
Bilal Mohamed-Benkada <bmbenkada@gmail.com>
2024-02-07 15:11:20 +0000

    Adding paramaters. Parameters*

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,11 @@
+using System.Data;
 using System.Security.Cryptography;
+using System.Text;
 using DotnetAPI.Data;
 using DotnetAPI.DTOs;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace DotnetAPI.Controllers
 {
@@ -31,7 +35,36 @@ namespace DotnetAPI.Controllers
                         rng.GetNonZeroBytes(passwordSalt);
                     }
 
-                    string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey"); //Retrieval
+                    string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value +
+                    Convert.ToBase64String(passwordSalt); //Retrieval + appendage
+
+                    byte[] passwordHash = KeyDerivation.Pbkdf2(
+                        password: userForRegistration.Password,
+                        salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
+                        prf: KeyDerivationPrf.HMACSHA256, // Pseudo Random Function
+                        iterationCount: 100000,
+                        numBytesRequested: 256 / 8
+
+                    );
+
+                    string sqlAuth = @"
+                    INSERT INTO TutorialAppSchema.Auth ([Email],
+                    [PasswordHash],
+                    [PasswordSalt]) VALUES ('" + userForRegistration.Email +
+                    "', @PasswordHash, @PasswordSalt)"; // Creates a variable in SQL
+
+                    List<SqlParameter> sqlParameters = new List<SqlParameter>();
+
+                    SqlParameter passwordSaltParameter = new SqlParameter("@PasswordSalt", SqlDbType.VarBinary);
+                    passwordSaltParameter.Value = passwordSalt;
+
+                    SqlParameter passwordHashParameter = new SqlParameter("@PasswordSalt", SqlDbType.VarBinary);
+                    passwordSaltParameter.Value = passwordSalt;
+
+                    sqlParameters.Add(passwordSaltParameter);
+                    sqlParameters.Add(passwordHashParameter);
+
+
                     return Ok();
                 }
                 throw new Exception("User with this email already exists");
